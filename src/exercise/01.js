989b8e0c5477795cd0a0e73fe2adf707a0ad7296
Steven <stevenkabc@redwood6.com>
2020-10-23 14:21:01 -0400

    useState reducer dereference count from object for html, store count as property of state

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,20 +2,23 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import React from 'react';
-function countReducerDispatch(currentCountState, step) {
-  return currentCountState + step;
+function countReducer(state, newCountState) {
+  console.log({...state, newCountState});
+  console.log({...state, ...newCountState});
+  return {...state, ...newCountState};
 }
 const {useReducer} = React;
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, changeCount] = useReducer(countReducerDispatch, initialCount);
+  const [state, setState] = useReducer(countReducer, {count: initialCount});
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => changeCount(step);
+  const {count} = state;
+  const increment = () => setState({count: count + step});
 
   return <button onClick={increment}>{count}</button>;
 }
