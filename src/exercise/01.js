13ec73773fc490d81e66e9edf62d4a9ae7e7c8f8
Steven <stevenkabc@redwood6.com>
2020-10-23 16:24:52 -0400

    cleanup extra credit 02 using state object {count: 1}

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,26 +2,16 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import React from 'react';
-function countReducer(state, newCountState) {
-  console.log({...state, newCountState});
-  console.log({...state, ...newCountState});
-  return {...state, ...newCountState};
+function countReducer(state, action) {
+  return {...state, ...action};
 }
 const {useReducer} = React;
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
+const Counter = ({initialCount = 0, step = 1}) => {
   const [state, setState] = useReducer(countReducer, {count: initialCount});
-
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
   const {count} = state;
   const increment = () => setState({count: count + step});
-
   return <button onClick={increment}>{count}</button>;
-}
+};
 function App() {
   return <Counter />;
 }
